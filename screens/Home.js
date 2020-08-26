be074f5ea33c41c21502a8239c40f563f1f6a237
Keshav Ashiya <keshavashiya@outlook.com>
2020-08-26 19:06:06 +0530

    List music done

--- screens/Home.js
+++ screens/Home.js
@@ -4,17 +4,23 @@ import {
   Text,
   ScrollView,
   TextInput,
-  FlatList,
   Alert,
   SafeAreaView,
+  ActivityIndicator,
+  Image,
+  Button,
+  Dimensions,
 } from 'react-native';
 const axios = require('axios');
 
+const {height, width} = Dimensions.get('window');
 const Home = () => {
   const [value, setValue] = useState('');
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
+  const search = () => {
+    setLoading(true);
     axios
       .get('https://itunes.apple.com/search', {
         params: {
@@ -22,66 +28,125 @@ const Home = () => {
         },
       })
       .then(function (response) {
-        // console.log(response);
-        setData(response.results);
+        if (response) {
+          setData(response.data.results);
+          setLoading(false);
+        }
       })
       .catch(function (error) {
-        // console.log(error);
         Alert.alert(error);
       })
       .then(function () {
         // always executed
       });
-  }, [value]);
-
-  const search = (text) => {
-    // console.log(text);
-    setValue(text);
-  };
-
-  const renderItem = ({item}) => {
-    console.log(item);
-    return (
-      <View>
-        <Text>{item.trackName}</Text>
-      </View>
-    );
   };
   return (
-    <ScrollView style={{flex: 1, flexDirection: 'column', padding: 32}}>
+    <ScrollView style={{flex: 1, padding: 32, backgroundColor: '#fff'}}>
       <SafeAreaView />
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          //   backgroundColor: 'red',
-          padding: 16,
-        }}>
-        <Text style={{fontWeight: 'bold', fontSize: 22}}>iTunes Music</Text>
-      </View>
       <View>
-        <TextInput
-          placeholder="Search..."
-          style={{height: 40, borderColor: 'gray', borderWidth: 1, padding: 12}}
-          onChangeText={(text) => search(text)}
-          value={value}
-        />
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            padding: 16,
+          }}>
+          <Text style={{fontWeight: 'bold', fontSize: 22}}>iTunes Music</Text>
+        </View>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <TextInput
+            placeholder="Search..."
+            style={{
+              height: 40,
+              borderColor: '#999998',
+              borderWidth: 1,
+              padding: 12,
+              borderRadius: 5,
+              flex: 1,
+            }}
+            onChangeText={(text) => setValue(text)}
+            value={value}></TextInput>
+          <View style={{marginLeft: 12, marginRight: 2}}>
+            <Button
+              onPress={search}
+              color="#999998"
+              type="outline"
+              title="Search"
+            />
+          </View>
+        </View>
       </View>
-      <View>
-        {data && (
-          <FlatList
-            data={data}
-            // renderItem={renderItem}
-            keyExtractor={(item) => item.id}
-            renderItem={({item}) => (
-              <View>
-                <Text>{item.trackName}</Text>
-              </View>
-            )}
-          />
-        )}
+      {!loading && data.length === 0 ? (
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            justifyContent: 'center',
+            marginTop: 32,
+            alignItems: 'center',
+          }}>
+          <Text>No Music</Text>
+        </View>
+      ) : null}
+      {loading && (
+        <View
+          style={{
+            flex: 1,
+            flexDirection: 'column',
+            marginTop: 32,
+          }}>
+          <ActivityIndicator color="#000" size="large" />
+        </View>
+      )}
+      <View style={{marginBottom: 52}}>
+        {data &&
+          data.map((item, index) => {
+            if (item.trackName) {
+              return (
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: 'row',
+                    marginTop: 16,
+                    height: 80,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                  key={index}>
+                  <View style={{flexDirection: 'column'}}>
+                    <Image
+                      style={{height: 80, width: 80, borderRadius: 10}}
+                      source={{uri: item.artworkUrl100}}></Image>
+                  </View>
+                  <View
+                    style={{
+                      flex: 1,
+                      flexDirection: 'column',
+                      marginLeft: 16,
+                    }}>
+                    <Text numberOfLines={1}>{item.trackName}</Text>
+                    <Text
+                      style={{marginTop: 4, color: '#999998'}}
+                      numberOfLines={1}>
+                      {item.artistName}
+                    </Text>
+                  </View>
+                  <View style={{flexDirection: 'column', marginLeft: 16}}>
+                    <Text style={{color: '#999998'}} numberOfLines={1}>
+                      {item.trackPrice} $
+                    </Text>
+                  </View>
+                </View>
+              );
+            }
+          })}
       </View>
     </ScrollView>
   );
